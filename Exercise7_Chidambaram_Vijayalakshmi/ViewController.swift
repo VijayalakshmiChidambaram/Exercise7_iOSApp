e01252e4724a433d09f7a1a8142df055e30cc974
VijayalakshmiChidambaram <vijayalakshmi1994.kc@gmail.com>
2021-11-04 23:51:30 -0500

    Initial Commit

---
+++ Exercise7_Chidambaram_Vijayalakshmi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Exercise7_Chidambaram_Vijayalakshmi
+//
+//  Created by student on 11/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
